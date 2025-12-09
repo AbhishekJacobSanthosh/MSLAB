97bb06d985c23c382178e97935213fe51b4be567
ox1g3n <aagnikg2003@gmail.com>
2025-12-09 16:37:02 +0000

    testing push

--- user-service/src/main/java/com/unigig/user/User.java
+++ user-service/src/main/java/com/unigig/user/User.java
@@ -5,7 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
+//we define the model here
 @Entity
 @Table(name = "users")
 public class User {
